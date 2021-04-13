1aa44fd80b2a6b5ef189d7beba5e8918e0ab3c1d
Raiana Sousa dos Santos <53270687+raianasousa@users.noreply.github.com>
2021-04-13 17:14:38 -0300

    Add files via upload

---
+++ aula02.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Aul02{
+class Programa {
+
+
+ public static void Main(string[] args) {
+    Console.WriteLine ("CFB Cursos");
+    if(args.GetLength(0)>0){
+    Console.WriteLine (args.GetValue(0));
+            }
+        }
+    }
+}
